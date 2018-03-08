404460af45845a7127e4735e58ca9f1b4941ec27
Pirlamps <matheus.e.sambiase@gmail.com>
2018-03-08 01:39:33 -0300

    first function for test

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,8 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+exports.duplicatePost = functions.database.ref('/social/post/matheus-1250')
+    .onWrite(event => {
+        const post = event.data.val();
+        return functions.database.ref('/social/duplicate/').set(post);
+    });
