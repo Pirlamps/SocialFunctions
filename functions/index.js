badfe77b877363eea7ba593b990c61598afd9604
Matheus Sambiase <matheus.sambiase@concrete.com.br>
2018-03-08 18:52:57 -0300

    mapping followers

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,32 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
 exports.duplicatePost = functions.database.ref('/social/post/matheus-1250')
     .onWrite(event => {
         const post = event.data.val();
-        return event.data.ref.root.child('/social/duplicate/').set(post);
+        
+        const functions = require('firebase-functions');
+        const admin = require('firebase-admin');
+        const config = functions.config();
+
+        admin.initializeApp(config.firebase);
+        
+        var followersMap
+        
+        const followers = admin.database().ref('/followers/matheus-1250/').once('value')
+            .then(snapshot =>{
+                followersMap = snapshot.data.val()//aqui ta zuado
+        });
+        
+        
+        var iterator = followersMap.keys()
+
+
+        return event.data.ref.root.child(`/social/duplicate/${iterator.next().value}`).set(post);
     });
+
+//      var post = "oie"
+// new Map([['yasmin','post'],['matheus','post']]).forEach((value, key, map) => {
+// 	map.set(key, post)
+//     console.log(`[${key}] = ${map.get(key)}`)
+// })
