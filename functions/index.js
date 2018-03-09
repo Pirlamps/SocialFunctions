4eb6150d7026e31df6e2ee93f6e4278073dc412e
Pirlamps <matheus.e.sambiase@gmail.com>
2018-03-09 00:37:28 -0300

    fully working duplicate function, first version

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,31 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const config = functions.config()
+//Initializing firebase admin to read from another node.
+admin.initializeApp(config.firebase)
 
-
+//Function that duplicate the current post to all followers
 exports.duplicatePost = functions.database.ref('/social/post/matheus-1250')
     .onWrite(event => {
-        const post = event.data.val();
+        const post = event.data.val()
         
-        const functions = require('firebase-functions');
-        const admin = require('firebase-admin');
-        const config = functions.config();
+        //Get the followers
+        return admin.database().ref('social/followers/matheus-1250').once('value')
+            .then(snapshot => {
+                const followersMap = snapshot.val()
+                console.log(followersMap)
 
-        admin.initializeApp(config.firebase);
-        
-        var followersMap
-        
-        const followers = admin.database().ref('/followers/matheus-1250/').once('value')
-            .then(snapshot =>{
-                followersMap = snapshot.data.val()//aqui ta zuado
-        });
-        
-        
-        var iterator = followersMap.keys()
+                //Transform the Map of keys into a JSON with the data to update.
+                var multiUpdate = {}
+                for(var k in followersMap){
+                    multiUpdate[k] = post
+                }
+                console.log(multiUpdate)
 
-
-        return event.data.ref.root.child(`/social/duplicate/${iterator.next().value}`).set(post);
-    });
-
-//      var post = "oie"
-// new Map([['yasmin','post'],['matheus','post']]).forEach((value, key, map) => {
-// 	map.set(key, post)
-//     console.log(`[${key}] = ${map.get(key)}`)
-// })
+                //Update all childrens in multiUpdate JSON
+                return event.data.ref.root.child('social/timeline/').update(multiUpdate)
+            }).catch(error => {
+                console.error(error)
+            })
+    })
+    //TODO: change event Reference to get all users
